ac5a3ef0f1526e7231a372400064119a9463cebf
Chris Boertien <chris@aktionlab.com>
2012-11-27 05:46:30 -0700

    add better failure messages and check that file is not a directory
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- lib/aktion_test/matchers/file_system/be_a_file.rb
+++ lib/aktion_test/matchers/file_system/be_a_file.rb
@@ -11,17 +11,33 @@ module AktionTest
 
         def matches?(subject)
           @subject = subject
-          File.exists? @subject
+          file_exists? and file_is_not_a_directory?
         end
 
       protected
-        
+
         def expectation
-          "#{@subject} to be a file"
+          "#{@subject} to be a file."
         end
 
         def problem
-          ""
+          if File.exists?(@subject)
+            if File.directory?(@subject)
+              "#{@subject} is a directory."
+            else
+              "Unknown"
+            end
+          else
+            "#{@subject} does not exist."
+          end
+        end
+
+        def file_exists?
+          File.exists? @subject
+        end
+
+        def file_is_not_a_directory?
+          !File.directory? @subject
         end
       end
     end
--- spec/matchers/be_a_file_spec.rb
+++ spec/matchers/be_a_file_spec.rb
@@ -1,15 +1,62 @@
 require 'spec_helper'
 
 describe AktionTest::Matchers::FileSystem::FileExistanceMatcher do
-  it "should accept an existing file" do
-    __FILE__.should be_a_file 
+  context "an existing file" do
+    let(:file) { __FILE__ }
+
+    it "will be accepted" do
+      file.should be_a_file
+    end
+
+    it "provides a negative failure message" do
+      matcher = described_class.new
+      matcher.matches?(file)
+      matcher.negative_failure_message.should == <<-MSG.strip_heredoc.strip
+        Did not expect #{file} to be a file.
+      MSG
+    end
+
+    it "provides a failure message with an unknown problem" do
+      matcher = described_class.new
+      matcher.matches?(file)
+      matcher.failure_message.should == <<-MSG.strip_heredoc.strip
+        Expected #{file} to be a file.
+        Unknown
+      MSG
+    end
   end
 
-  it "should not accept a non-existant file" do
-    File.join(__FILE__, '..', 'foo').should_not be_a_file
+  context "a non-existant file" do
+    let(:file) { File.expand_path(File.join(__FILE__, '..', 'foo')) }
+
+    it "will not be accepted" do
+      file.should_not be_a_file
+    end
+
+    it "explains that the subject does not exist" do
+      matcher = described_class.new
+      matcher.matches?(file)
+      matcher.failure_message.should == <<-MSG.strip_heredoc.strip
+        Expected #{file} to be a file.
+        #{file} does not exist.
+      MSG
+    end
   end
 
-  it "should not accept a directory" do
-    File.join(__FILE__, '..').should_not be_a_file
+  context "an existing directory" do
+    let(:file) { File.expand_path(File.join(__FILE__, '..')) }
+
+    it "will not be accpeted" do
+      file.should_not be_a_file
+    end
+
+    it "explains that the subject is a directory" do
+      matcher = described_class.new
+      matcher.matches?(file)
+      matcher.failure_message.should == <<-MSG.strip_heredoc.strip
+        Expected #{file} to be a file.
+        #{file} is a directory.
+      MSG
+    end
   end
 end
