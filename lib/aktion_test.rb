4a20179207225b7ffcfed46f26fff40b6e104ca0
Chris Boertien <chris@aktionlab.com>
2012-11-27 04:54:33 -0700

    add class builder for creating classes on the fly
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- lib/aktion_test.rb
+++ lib/aktion_test.rb
@@ -1,4 +1,7 @@
+require 'active_support/dependencies'
+
 require "aktion_test/version"
 require "aktion_test/matchers/base"
+require 'aktion_test/class_builder'
 require "aktion_test/matchers/integrations/rspec"
 
---
+++ spec/aktion_test/class_builder_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe AktionTest::ClassBuilder do
+  it "creates a new class" do
+    clazz = define_class('Foo')
+    clazz.new.should be_a Foo
+  end
+
+  it "creates a new class with a base" do
+    base = define_class('Foo')
+    clazz = define_class('Bar', Foo)
+    clazz.superclass.should == Foo
+  end
+
+  it "creates a new class and class evals a block" do
+    clazz = define_class('Foo') do
+      class << self
+        def foo
+          "baz"
+        end
+      end
+
+      attr_accessor :bar
+    end
+
+    clazz.foo.should == 'baz'
+    clazz.new.should respond_to :bar
+    clazz.new.should respond_to :bar=
+  end
+end
