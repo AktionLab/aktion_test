9222a4e3d060fe4ee739741938235775cf76669f
Chris Boertien <chris@aktionlab.com>
2013-01-30 08:09:22 -0700

    add factory girl syntax to rspec when its loaded
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- lib/aktion_test/module/factory_girl.rb
+++ lib/aktion_test/module/factory_girl.rb
@@ -5,6 +5,10 @@ module AktionTest
       
       included do |spec_helper|
         require 'factory_girl'
+
+        ::RSpec.configure do |config|
+          config.include ::FactoryGirl::Syntax::Methods
+        end
       end
     end
   end
