753b3d229533ffdd7ef88803bb73af6e54a6e680
Chris Boertien <chris@aktionlab.com>
2013-01-30 01:08:23 -0700

    implement more detail in the base matcher
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- lib/aktion_test/matchers/base.rb
+++ lib/aktion_test/matchers/base.rb
@@ -1,12 +1,44 @@
 module AktionTest
   module Matchers
     class Base
+      def initialize
+        @matches = nil
+      end
+
+      def matches?(subject)
+        return @matches unless @matches.nil?
+        @subject = subject
+        @matches = !!perform_match!
+      end
+
       def failure_message
-        "Expected #{expectation}\n#{problem}"
+        raise "Called failure message before determining a match from #{caller[0]}" if @matches.nil?
+        raise "Called failure message while the match was positive from #{caller[0]}" if @matches
+        "Expected #{expectation}\n#{problems_for_should}\n"
       end
 
       def negative_failure_message
-        "Did not expect #{expectation}"
+        raise "Called negative failure message before determining a match from #{caller[0]}" if @matches.nil?
+        raise "Called negative failure message while the match was unsucessful from #{caller[0]}" unless @matches
+        "Did not expect #{expectation}\n#{problems_for_should_not}\n"
+      end
+
+    protected
+
+      def expectation
+        "Override expectation to provide expectation details"
+      end
+      
+      def problems_for_should
+        "Override problem_for_should to set problems in the failure message output."
+      end
+
+      def problems_for_should_not
+        "Override problem_for_should_not to set problems in the failure message output."
+      end
+
+      def perform_match!
+        raise "Override perform_match! with your custom matching logic. The subject is available through @subject"
       end
     end
   end
--- spec/matchers/base_spec.rb
+++ spec/matchers/base_spec.rb
@@ -1,51 +1,59 @@
 require 'spec_helper'
 
 describe AktionTest::Matchers::Base do
-  it "requires the extending class to implement #expectation" do
-    matcher = define_class('Matcher', described_class)
-    expect { matcher.new.failure_message }.to raise_error(NameError, /`expectation'/)
-    expect { matcher.new.negative_failure_message }.to raise_error(NameError, /`expectation'/)
-  end
-
-  it "requires the extending class to implement #problem" do
-    matcher = define_class('Matcher', described_class) do
-    protected
-      def expectation
-        ''
-      end
+  before :each do
+    matcher_class = define_class('Matcher', described_class) do
+      def expectation;         "an expectation";               end
+      def problems_for_should; "\na problem\nanother problem"; end
+      def problems_for_should_not; "\na problem\nanother problem"; end
     end
 
-    expect { matcher.new.failure_message }.to raise_error(NameError, /`problem'/)
+    @matcher = matcher_class.new
   end
+    
+  describe '#failure_message' do
+    it "outputs an expectation with associated problems when the matcher is unsuccessful" do
+      @matcher.stub(:perform_match! => false)
+      @matcher.matches?(nil)
+      @matcher.failure_message.should == <<-MSG.strip_heredoc
+        Expected an expectation
 
-  it "provides a failure message based on #expectation and #problem" do
-    matcher = define_class('Matcher', described_class) do
-    protected
-      def expectation
-        'an expectation'
-      end
-
-      def problem
-        'A Problem'
-      end
+        a problem
+        another problem
+      MSG
     end
 
-    matcher.new.failure_message.should == <<-MSG.strip_heredoc.strip
-      Expected an expectation
-      A Problem
-    MSG
-  end
-
-  it "provides a negative failure message based on #expectation" do
-    matcher = define_class('Matcher', described_class) do
-    protected
-      def expectation
-        'an expectation'
-      end
+    it "raises an error if called before running the matcher" do
+      expect { @matcher.failure_message }.to raise_error(RuntimeError, /failure message before/)
     end
 
-    matcher.new.negative_failure_message.should == <<-MSG.strip_heredoc.strip
-      Did not expect an expectation
-    MSG
+    it "raises an error if called when the matcher was successful" do
+      @matcher.stub(:perform_match! => true)
+      @matcher.matches? nil
+      expect { @matcher.failure_message }.to raise_error(RuntimeError, /failure message while/)
+    end
+  end
+
+  describe '#negative_failure_message' do
+    before { @matcher.stub(:perform_match! => true) }
+    it 'outputs a negative expectation with associated problems when the matcher is successful' do
+      @matcher.matches?(nil)
+      @matcher.negative_failure_message.should == <<-MSG.strip_heredoc
+        Did not expect an expectation
+
+        a problem
+        another problem
+      MSG
+    end
+
+    it "raises an error if called before running the matcher" do
+      expect { @matcher.negative_failure_message }.to raise_error(RuntimeError, /negative failure message before/)
+    end
+
+    it "raises an error if called when the matcher was unsuccessful" do
+      @matcher.stub(:perform_match! => false)
+      @matcher.matches? nil
+      expect { @matcher.negative_failure_message }.to raise_error(RuntimeError, /negative failure message while/)
+    end
   end
 end
